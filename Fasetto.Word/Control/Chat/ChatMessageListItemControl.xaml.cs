b7ece2e33f82e8e5369f89ca8af7344650a39cf1
Gary Wu <gary.wu_1939@outlook.com>
2021-02-15 21:50:51 -0500

    - 14.3 创建 ChatMessage Bubble
        创建 ChatMessage Bubble 需要考虑的东西非常多，首先你要在 Chat Page 当中能够显示文字、视频、图片、多个视频。同时图片没有边框，视频能够在 ChatPage 中直接播放。这里面的步骤相当复杂，因此 ChatMessageListItemControl 其实是一个 Container，然后根据不同的 message 类型，向这个 ListItem Control 中注入不同的内容。
    
        1. 从 ChatMessageControl 复制一份作为基础样本，创建一个 ChatMessageListControl
        2. 创建一个 ChatMessageListItemControl
        3. 将 ChatMessageListControl 的 ItemSource 设置为 ChatMessageListItemControl，然后将其添加到 ChatMessage 当中。理论上你能够看到一系列的 ChatMessageListItemControl。
    
        - 14.3.1 设置 Bubble
            1. 设置 Border 为半圆，背景色为蓝色
            2. 设置 Bubble 中的 Message 样式，其实也就是一个 TextBlock
            3. 设置 Profile Picture，注意它是一个圆形的 Border 构成的
            4. 设置 Bubble 上那个小角，使用的 [Path Markup Syntax](https://docs.microsoft.com/en-us/dotnet/desktop/wpf/graphics-multimedia/path-markup-syntax?view=netframeworkdesktop-4.8) , 这个可以通过起始点和终点来定义一条线，然后可以设置 Fill 来设置该线所谓起来的图形的颜色
    
                ```java
                <Path Stroke="Black"
                      Grid.Row="1"
                      Margin="15 0 15 0"
                      Panel.ZIndex="1"
                      Data="M 0,-2 L 8,8 L 20,-2"
                      StrokeThickness="0"
                      Fill="{StaticResource ForegroundLightBrush}" />
                ```
    
            5. 添加时间

---
+++ Fasetto.Word/Control/Chat/ChatMessageListItemControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fasetto.Word.Control.Chat
+{
+    /// <summary>
+    /// Interaction logic for ChatMessageListItemControl.xaml
+    /// </summary>
+    public partial class ChatMessageListItemControl : UserControl
+    {
+        public ChatMessageListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
