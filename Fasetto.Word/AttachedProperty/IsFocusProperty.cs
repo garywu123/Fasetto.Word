a05788540d75d38e5c7e80e04871601dc198809b
Gary Wu <gary.wu_1939@outlook.com>
2021-02-19 22:24:45 -0500

    - 14.5 给 Login Page 上的 Email 输入框设置默认聚焦，并为 Login Button 设置 Enter 按钮触发
        当 APP 运行了以后，第一个界面 LoginPage 上的 Email 并不会自动聚焦，需要手动的去点一下 Email 从而光标会显示在上面。我们希望实现默认聚焦。
    
        实现聚焦的方式有多种，比如：
    
        1. 设置 Focusmanager，在对应的元素上设置 Focusmanager，让其在显示的时候默认获得聚焦
    
            ```java
            <Grid FocusManager.FocusedElement="{Binding ElementName=emailTextBox}">
            ```
    
            但是 Visual Focus，它的确会让光标显示在上面，但光标并不会闪动，因为这只是视觉上的 focus，并不是实际上的 keyboard focus。
    
        2. 添加依赖属性 IsFocusProperty
    
            这个依赖属性可以让控件拥有 Focus 属性，只要当 Email 的 IsFocusProperty 属性为 True 的时候，就会触发 ValueChanghed 事件，然后 TextBox 一旦 loaded，就会触发 loaded 事件，修改 Textbox 的 Focus 属性。
    
            ```java
            public class IsFocusProperty : BaseAttachedProperty<IsFocusProperty, bool>
            {
                public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
                {
                    base.OnValueChanged(sender, e);
    
                    // if we don't have a control return
                    if (!(sender is System.Windows.Controls.Control control))
                    {
                        return;
                    }
                    // Focus this control once loaded.
                    control.Loaded += (s, se) => control.Focus();
                }
            }
            ```
    
        3. 将 Login Button 的 IsDefault 属性设置为 True，那么用户只要按下 Enter 就默认是按下了该按钮。

---
+++ Fasetto.Word/AttachedProperty/IsFocusProperty.cs
@@ -0,0 +1,33 @@
+﻿#region License
+
+// author:         吴经纬
+// created:        22:31
+// description:
+
+#endregion
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Fasetto.Word.AttachedProperty
+{
+    /// <summary>
+    ///     Focuses (keyboard focus) this element on load
+    /// </summary>
+    public class IsFocusProperty : BaseAttachedProperty<IsFocusProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            base.OnValueChanged(sender, e);
+
+            // if we don't have a control return
+            if (!(sender is System.Windows.Controls.Control control))
+            {
+                return;
+            }
+            // Focus this control once loaded.
+            control.Loaded += (s, se) => control.Focus();
+        }
+    }
+}
